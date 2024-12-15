4f24fa2f6679eb4e25085474e063086cd53e4348
edward-baker05 <ed9baker@outlook.com>
2024-12-15 15:16:20 +0000

    Undid one previous change

--- init.lua
+++ init.lua
@@ -436,21 +436,6 @@ require('lazy').setup {
     config = function()
       vim.cmd.colorscheme 'nord'
     end,
-    require("headlines").setup({
-      markdown = {
-        headline_highlights = {
-          "Headline1",
-          "Headline2",
-          "Headline3",
-          "Headline4",
-          "Headline5",
-          "Headline6",
-        },
-        codeblock_highlight = "CodeBlock",
-        dash_highlight = "Dash",
-        quote_highlight = "Quote",
-      },
-    })
   },
 
   -- Highlight todo, notes, etc in comments
