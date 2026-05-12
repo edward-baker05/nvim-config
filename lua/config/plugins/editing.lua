ce45c65ea104b59fa23e50cbb621c0b01684d87e
Edward Baker <ed9baker@outlook.com>
2026-05-12 13:38:02 +0100

    feat: add lazygit.nvim and vim-fugitive

--- lua/config/plugins/editing.lua
+++ lua/config/plugins/editing.lua
@@ -58,6 +58,26 @@ return {
 		},
 	},
 	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "[L]azy[G]it" },
+		},
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
 		branch = "main",
 		lazy = false,
